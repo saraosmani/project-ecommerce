ce6afecac8a2e13cc94e7977bab2c86e4787459f
saraosmani <saraosmani90@gmail.com>
2023-09-29 15:24:32 +0200

    navbar fixed

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -17,14 +17,14 @@ import { useQuery, gql } from '@apollo/client';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 interface Category {
-  id: number;
-  title: string;
+    id: number;
+    title: string;
 }
 
 interface Subcategory {
-  subcategory_image_url: string;
-  subcategory_name: string;
-  category_id: number;
+    subcategory_image_url: string;
+    subcategory_name: string;
+    category_id: number;
 }
 
 const CATEGORIES = gql`
@@ -47,129 +47,133 @@ const SUBCATEGORY = gql`
 `;
 
 function Navbar() {
-  const { categoryTitle } = useParams();
-  const [drawer, setDrawer] = useState(false);
-  const [hoveredCategory, setHoveredCategory] = useState<string | null>(null);
+    const { categoryTitle } = useParams();
+    const [drawer, setDrawer] = useState(false);
+    const [hoveredCategory, setHoveredCategory] = useState<string | null>(null);
 
-  const {  data } = useQuery<{ categories: Category[] }>(CATEGORIES, {
-    variables: {
-      title: categoryTitle,
-    },
-  });
-
-  const {  data: subcategoriesData } =
-    useQuery<{ subcategory_categories_view: Subcategory[] }>(SUBCATEGORY, {
-      variables: {
-        title: hoveredCategory || '', // Use the hovered category to fetch subcategories
-      },
-      skip: !hoveredCategory, // Skip the query if there's no hovered category
+    const { data } = useQuery<{ categories: Category[] }>(CATEGORIES, {
+        variables: {
+            title: categoryTitle,
+        },
     });
 
-  const handleCategoryMouseEnter = (title: string) => {
-    setHoveredCategory(title);
-  };
+    const { data: subcategoriesData } =
+        useQuery<{ subcategory_categories_view: Subcategory[] }>(SUBCATEGORY, {
+            variables: {
+                title: hoveredCategory || '', // Use the hovered category to fetch subcategories
+            },
+            skip: !hoveredCategory, // Skip the query if there's no hovered category
+        });
 
-  const handleCategoryMouseLeave = () => {
-    setHoveredCategory(null);
-  };
+    const handleCategoryMouseEnter = (title: string) => {
+        setHoveredCategory(title);
+    };
 
-  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-    if (
-      ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
-    ) {
-      return;
-    }
+    const handleCategoryMouseLeave = () => {
+        setHoveredCategory(null);
+    };
 
-    setDrawer(open);
-  };
+    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+            ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+            return;
+        }
 
-  return (
-    <div>
-      <AppBar position="fixed" sx={{ width: '100%' }}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
-          <IconButton color="inherit">
-            <AccountCircleIcon />
-          </IconButton>
-          <IconButton color="inherit">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton color="inherit">
-            <ShoppingCartIcon />
-          </IconButton>
-          <IconButton color="inherit" onClick={toggleDrawer(true)} edge="start">
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer anchor="right" open={drawer} onClose={toggleDrawer(false)}>
-        <div
-          role="presentation"
-          onClick={toggleDrawer(false)}
-          onKeyDown={toggleDrawer(false)}
-        >
-        <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginRight: "20px",marginTop: "15px"}}>
-      <IconButton color="inherit" onClick={toggleDrawer(false)} edge="end" >
-        <CloseIcon />
-      </IconButton>
-    </div>
-          <List sx={{ width: '350px' }}>
-            {data?.categories.map((category) => (
-              <div
-                key={category.id}
-                onMouseEnter={() => handleCategoryMouseEnter(category.title)}
-                onMouseLeave={handleCategoryMouseLeave}
-              >
-                <ListItem
-                  button
-                  key={category.id}
-                  component={Link}
-                  to={`/category/${category.title.toLowerCase()}`}
+        setDrawer(open);
+    };
+
+    return (
+        <div>
+            <AppBar position="fixed" sx={{ width: '100%' }}>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
+                    <IconButton color="inherit" sx={{ marginRight: '8px' }}>
+                        <AccountCircleIcon />
+                    </IconButton>
+                    <IconButton color="inherit" sx={{ marginRight: '8px' }}>
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton color="inherit" sx={{ marginRight: '8px' }}>
+                        <ShoppingCartIcon />
+                    </IconButton>
+                    <IconButton color="inherit" onClick={toggleDrawer(true)} edge="start" sx={{
+                        marginLeft: '10px',
+                        boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.5)',
+                       
+                    }}>
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer anchor="right" open={drawer} onClose={toggleDrawer(false)}>
+                <div
+                    role="presentation"
+                    onClick={toggleDrawer(false)}
+                    onKeyDown={toggleDrawer(false)}
                 >
-                  <ListItemText
-                    primary={
-                      <Typography variant="h6" fontWeight="bold">
-                        {category.title.charAt(0).toUpperCase() + category.title.slice(1)}
-                      </Typography>
-                    }
-                  />
-                  {hoveredCategory === category.title ? (
-                    <ExpandMoreIcon />
-                  ) : null}
-                </ListItem>
-                {hoveredCategory === category.title && (
-                  <List
-                    sx={{
-                      display: 'block',
-                      '&:hover': {
-                        display: 'block',
-                      },
-                    }}
-                  >
-                    {subcategoriesData?.subcategory_categories_view.map((subcategory) => (
-                      <ListItem
-                        button
-                        key={subcategory.category_id}
-                        component={Link}
-                        to={`/category/${category.title.toLowerCase()}/${subcategory.subcategory_name.toLowerCase()}`}
-                      >
-                        <ListItemText primary={
-                        <Typography variant="subtitle1"> 
-                        {subcategory.subcategory_name.charAt(0).toUpperCase() + subcategory.subcategory_name.slice(1)} 
-                        </Typography>
-                        }
-                        />
-                      </ListItem>
-                    ))}
-                  </List>
-                )}
-              </div>
-            ))}
-          </List>
+                    <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginRight: "20px", marginTop: "15px" }}>
+                        <IconButton color="inherit" onClick={toggleDrawer(false)} edge="end" >
+                            <CloseIcon />
+                        </IconButton>
+                    </div>
+                    <List sx={{ width: '350px' }}>
+                        {data?.categories.map((category) => (
+                            <div
+                                key={category.id}
+                                onMouseEnter={() => handleCategoryMouseEnter(category.title)}
+                                onMouseLeave={handleCategoryMouseLeave}
+                            >
+                                <ListItem
+                                    button
+                                    key={category.id}
+                                    component={Link}
+                                    to={`/category/${category.title.toLowerCase()}`}
+                                >
+                                    <ListItemText
+                                        primary={
+                                            <Typography variant="h6" fontWeight="bold">
+                                                {category.title.charAt(0).toUpperCase() + category.title.slice(1)}
+                                            </Typography>
+                                        }
+                                    />
+                                    {hoveredCategory === category.title ? (
+                                        <ExpandMoreIcon />
+                                    ) : null}
+                                </ListItem>
+                                {hoveredCategory === category.title && (
+                                    <List
+                                        sx={{
+                                            display: 'block',
+                                            '&:hover': {
+                                                display: 'block',
+                                            },
+                                        }}
+                                    >
+                                        {subcategoriesData?.subcategory_categories_view.map((subcategory) => (
+                                            <ListItem
+                                                button
+                                                key={subcategory.category_id}
+                                                component={Link}
+                                                to={`/category/${category.title.toLowerCase()}/${subcategory.subcategory_name.toLowerCase()}`}
+                                            >
+                                                <ListItemText primary={
+                                                    <Typography variant="subtitle1">
+                                                        {subcategory.subcategory_name.charAt(0).toUpperCase() + subcategory.subcategory_name.slice(1)}
+                                                    </Typography>
+                                                }
+                                                />
+                                            </ListItem>
+                                        ))}
+                                    </List>
+                                )}
+                            </div>
+                        ))}
+                    </List>
+                </div>
+            </Drawer>
         </div>
-      </Drawer>
-    </div>
-  );
+    );
 }
 
 export default Navbar;
